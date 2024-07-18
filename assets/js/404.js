cfe6a32a3d20e3f3e70be8be4a304146101fa25f
kimhabork <kimhab.ork@yahoo.com>
2024-07-19 04:24:15 +0700

    update

---
+++ assets/js/404.js
@@ -0,0 +1,23 @@
+const anime = require("/assets/js/anime.min.js")
+
+anime({
+  targets: '.row svg',
+  translateY: 10,
+  autoplay: true,
+  loop: true,
+  easing: 'easeInOutSine',
+  direction: 'alternate'
+});
+
+anime({
+  targets: '#zero',
+  translateX: 10,
+  autoplay: true,
+  loop: true,
+  easing: 'easeInOutSine',
+  direction: 'alternate',
+  scale: [{value: 1}, {value: 1.4}, {value: 1, delay: 250}],
+    rotateY: {value: '+=180', delay: 200},
+});
+
+
